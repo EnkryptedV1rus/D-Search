f538ded8b8249da4b6c206eecc94cabea261f489
Nexo <134344861+EnkryptedV1rus@users.noreply.github.com>
2026-02-22 01:12:47 -0500

    Add initial Astro configuration file

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  output: 'static',
+  // If your site is in a subfolder, uncomment and set:
+  // base: '/D-Search',
+});
