3d96e5068fba5137900bf8d79ccd0608e758c87b
Nexo <134344861+EnkryptedV1rus@users.noreply.github.com>
2026-02-22 01:17:13 -0500

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  output: 'static',
-  // If your site is in a subfolder, uncomment and set:
-  // base: '/D-Search',
-});
